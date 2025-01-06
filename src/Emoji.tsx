02a40365ddb1178fde05d7ea7cf3096690e99c87
DarlanHenrique <darlan.silva@ice.ufjf.br>
2025-01-06 20:40:39 -0300

    create switch status button

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,39 +1,52 @@
 import "./Emoji.css";
 import { useState } from "react";
 
-const EMOJIS = new Map<string, string>(
-    [
-        ["happy", "😊"],
-        ["dead", "💀"],
-        ["programming", "👨‍💻"],
-    ],
-);
+const EMOJIS = new Map<string, string>([
+  ["happy", "😊"],
+  ["dead", "💀"],
+  ["programming", "👨‍💻"],
+]);
 //EMOJIS.set("happy", "😊");
 
-export default function Emoji() { 
+export default function Emoji() {
   const [situation, setSituation] = useState("");
 
   function toHappy() {
-    setSituation("happy");    
+    setSituation("happy");
   }
 
   function toDead() {
-    setSituation("dead");    
+    setSituation("dead");
   }
 
-function toWork() {
+  function toWork() {
     setSituation("programming");
-}
+  }
+
+ function switchStatus() {
+    switch (situation) { 
+    case "happy":
+        setSituation("dead");
+        break;
+    case "dead":
+        setSituation("programming");
+        break;
+    case "":
+    case "programming":
+        setSituation("happy");
+        break;
+    }
+  }
 
   return (
     <div className="emoji">
-        <div className="face">{EMOJIS.get(situation) || "🤡"}</div>
-        <div className="actions">
+      <div className="face">{EMOJIS.get(situation) || "🤡"}</div>
+      <div className="actions">
         <button onClick={toHappy}>Vivo</button>
         <button onClick={toDead}>Morto</button>
-        <button onClick={toWork}>Trabalhando</button>
-        </div>
+        <button onClick={toWork}>Programando</button>
+        <button onClick={switchStatus}>Alternar Situação</button>
+      </div>
     </div>
-)
+  );
 }
-
