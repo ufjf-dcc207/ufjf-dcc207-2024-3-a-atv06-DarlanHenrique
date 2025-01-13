53bd812ed6f77d6c830913bd6df3fbb10adf21db
DarlanHenrique <darlan.silva@ice.ufjf.br>
2025-01-13 20:23:36 -0300

    adding health logic

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -11,65 +11,75 @@ const EMOJIS = new Map<string, string>([
 
 export default function Emoji() {
   const [situation, setSituation] = useState("");
+  const [food, setFood] = useState(3);
+  const [water, setWater] = useState(3);
+  const [health, setHealth] = useState(3);
+  const [energy, setEnergy] = useState(3);
 
-//   function toHappy() {
-//     setSituation("happy");
-//   }
+  //   function toHappy() {
+  //     setSituation("happy");
+  //   }
 
-//   function toDead() {
-//     setSituation("dead");
-//   }
+  //   function toDead() {
+  //     setSituation("dead");
+  //   }
 
-//   function toWork() {
-//     setSituation("programming");
-//   }
+  //   function toWork() {
+  //     setSituation("programming");
+  //   }
 
-//  function switchStatus() {
-//     switch (situation) { 
-//         case "happy":
-//             setSituation("dead");
-//             break;
-//         case "dead":
-//             setSituation("programming");
-//             break;
-//         case "programming":
-//             setSituation("happy");
-//             break;
-//         default:
-//             setSituation("happy");
-//             break;
-//     }
-//   }
+  //  function switchStatus() {
+  //     switch (situation) {
+  //         case "happy":
+  //             setSituation("dead");
+  //             break;
+  //         case "dead":
+  //             setSituation("programming");
+  //             break;
+  //         case "programming":
+  //             setSituation("happy");
+  //             break;
+  //         default:
+  //             setSituation("happy");
+  //             break;
+  //     }
+  //   }
 
-  function onAlimentar() {
-
+  function onFood() {
+    setFood(Math.min(food + 1, 5));
   }
 
-  function OnHidratar() {
-
+  function onWater() {
+    setWater(Math.min(water + 1, 5));
   }
 
-  function onLigaDesligaLuz() {
-
+  function onEnergy() {
+    setEnergy(Math.min(energy + 1, 5));
   }
 
   function onCiclo() {
+    setFood(Math.max(0, food - 1));
+    setWater(Math.max(0, water - 1));
+    setEnergy(Math.max(0, energy - 1));
 
+    if (food === 0) setHealth((prevHealth) => Math.max(0, prevHealth - 1));
+    if (water === 0) setHealth((prevHealth) => Math.max(0, prevHealth - 1));
+    if (energy === 0) setHealth((prevHealth) => Math.max(0, prevHealth - 1));
   }
 
   return (
     <div className="emoji">
       <div className="face">{EMOJIS.get(situation) || "🤡"}</div>
       <div className="attributes">
-        <Attribute icon="💗" value={3} />
-        <Attribute icon="⚡" value={3} />
-        <Attribute icon="💧" value={3} />
-        <Attribute icon="🍖" value={3} />
+        <Attribute icon="💗" value={health} />
+        <Attribute icon="⚡" value={energy} />
+        <Attribute icon="💧" value={water} />
+        <Attribute icon="🍖" value={food} />
       </div>
       <div className="actions">
-        <button onClick={onAlimentar}>Alimentar</button>
-        <button onClick={OnHidratar}>Hidratar</button>
-        <button onClick={onLigaDesligaLuz}>Ligar/Desligar Luz</button>
+        <button onClick={onFood}>Alimentar</button>
+        <button onClick={onWater}>Hidratar</button>
+        <button onClick={onEnergy}>Ligar/Desligar Luz</button>
         <button onClick={onCiclo}>Ciclo</button>
       </div>
     </div>
